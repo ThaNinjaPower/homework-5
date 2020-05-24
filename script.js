98fb25ff64e841a327bb2c7c7c8add20a09c998f
ThaNinjaPower <kevinvannguyen1997@gmail.com>
2020-05-24 01:42:16 -0500

    Schedule cards now appear. Moment implementation not complete yet.

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    var currentDate = moment().format('MMMM Do YYYY');
+    $("#currentDay").text(currentDate);
+
+    var currentHour = moment().format('h');
+
+    var timeSlots = $(".time-block");
+    console.log(timeSlots);
+
+    $
+
+    $(".saveBtn").on("click", function() {
+
+    });
+})
