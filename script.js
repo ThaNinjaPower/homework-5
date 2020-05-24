36742a10eb81ab2d2f8cc281e8801d591c811961
ThaNinjaPower <kevinvannguyen1997@gmail.com>
2020-05-24 05:10:48 -0500

    Added color coding to the days to differentiate between past, present, and future.

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 $(document).ready(function() {
+    var savedSchedule;
+    if (localStorage.getItem("savedSchedule") !== undefined) {
+        savedSchedule = localStorage.getItem("savedSchedule");
+    }
+
     var currentDate = moment().format('MMMM Do YYYY');
+    var currentTime = moment().set('minute', 0).set('second', 0);
+
+    console.log(`Current: ${currentTime}`);
+
     $("#currentDay").text(currentDate);
 
-    var currentHour = moment().format('h');
+    var i = 0;
+    $(".time-block").each(function() {
+        var timeBlockTime = moment().set('hour', 9 + i).set('minute', 0).set('second', 0);
+        console.log(`Card date: ${timeBlockTime}`);
 
-    var timeSlots = $(".time-block");
-    console.log(timeSlots);
+        if (timeBlockTime.isSame(currentTime, 'hour')) {
+            $(this).find(".description").addClass("present");
+        }
+        else if (timeBlockTime.isBefore(currentTime)) {
+            $(this).find(".description").addClass("past");
+        }
+        else if (timeBlockTime.isAfter(currentTime)) {
+            $(this).find(".description").addClass("future");
+        }
 
-    $
+        console.log(`Same as current time? ${timeBlockTime.isSame(currentTime, 'hour')}`);
+
+        i++;
+    })
 
     $(".saveBtn").on("click", function() {
 
     });
-})
+});
